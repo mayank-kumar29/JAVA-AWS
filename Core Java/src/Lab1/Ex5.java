60d4e4761ba794bd0737786f17ca12b892278df5
mayank-kumar29 <71247904+mayank-kumar29@users.noreply.github.com>
2022-02-09 18:00:56 +0530

    Add files via upload

---
+++ Core Java/src/Lab1/Ex5.java
@@ -0,0 +1,38 @@
+package Lab1;
+import java.util.Scanner;
+public class Ex5 
+{
+	static int calculateSum(int n)
+	{
+		int sum=0,k;
+		for(int i=0;i<=n;i++) 
+		{
+			k=0;
+			for(int j=1;j<=i;j++)
+			{
+				if(i%3==0)
+				{
+					k++;
+				}
+				if(i%5==0)
+				{
+					k++;
+				}
+			}
+			if(k>0) 
+			{
+				sum=sum+i;
+			}
+		}
+		return sum;
+	}
+	public static void main(String[] args) 
+	{
+		int n;
+		System.out.println("Enter the number:");
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		System.out.println("Sum:"+calculateSum(n));
+	}
+
+}
