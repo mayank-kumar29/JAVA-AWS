7fd5e1cc7de9ac9f31b5bb62e0ea25478870da2d
mayank-kumar29 <71247904+mayank-kumar29@users.noreply.github.com>
2022-02-14 18:50:36 +0530

    Add files via upload

---
+++ Core Java/src/Lab4/MediaItem.java
@@ -0,0 +1,15 @@
+package Lab4;
+
+public abstract class MediaItem extends Item
+{
+	private int runtime;
+
+	public int getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+
+}
