91191e2c159ab5982141303d70982fd29f5d2a22
mayank-kumar29 <71247904+mayank-kumar29@users.noreply.github.com>
2022-02-24 08:51:01 +0530

    Delete Core Java directory

--- Core Java/src/com/cg/eis/exception/EmployeeException.java
+++
@@ -1,9 +0,0 @@
-package com.cg.eis.exception;
-
-public class EmployeeException extends Exception
-{
-	public EmployeeException(String str)
-	{
-		super(str);
-	}
-}
--- Core Java/src/lab5/UserDefinedException.java
+++
@@ -1,9 +0,0 @@
-package lab5;
-
-public class UserDefinedException extends Exception
-{
-	UserDefinedException(String str)
-	{
-		super(str);
-	}
-}
